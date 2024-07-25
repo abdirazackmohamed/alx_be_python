383e74a8dd339b9e5aeabeda0d4589b37fae8881
abdirazackmohamed <abdirazackmohamad@gmail.com>
2024-07-25 15:35:54 +0300

    modified the input to oneline

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,5 @@
-Monthly_Income = input("Enter your monthly income : ")
-Monthly_Income = int(Monthly_Income)
-Total_Monthly_Expense = input("Enter your total monthly expenses : ")
-Total_Monthly_Expense = int(Total_Monthly_Expense)
+Monthly_Income = int(input("Enter your monthly income : "))
+Total_Monthly_Expense = int(input("Enter your total monthly expenses : "))
 Monthly_savings = Monthly_Income - Total_Monthly_Expense
 projected_savings = Monthly_savings * 12 + (Monthly_savings * 12 * 0.05)
 print("Your monthly savings are $", Monthly_savings)
